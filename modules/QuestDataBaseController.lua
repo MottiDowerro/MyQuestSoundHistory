c6d9d05a10e377126e67dd3cb482a6887322a10f
MottiDowerro <explaycinematv3g@gmail.com>
2025-06-25 14:39:38 +0300

    tested

--- modules/QuestDataBaseController.lua
+++ modules/QuestDataBaseController.lua
@@ -1,11 +1,5 @@
 local function QuestDataBaseController_OnLoad()
-    print('я кошка мяу')
-    print('я кошка мяу')
-    print('я кошка мяу')
-    print('я кошка мяу')
-    print('я кошка мяу')
-    print('я кошка мяу')
-    print('я кошка мяу')
+
 end
 
 _G.QuestDataBaseController_OnLoad = QuestDataBaseController_OnLoad
